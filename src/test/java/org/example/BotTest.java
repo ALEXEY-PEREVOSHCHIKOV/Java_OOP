8d6dc27533918f7888fc7a81e93cd91fded0eabf
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-12-04 21:19:31 +0500

    добавлены новые тесты для обработки команд, работающих с классом BookVoting

--- src/test/java/org/example/BotTest.java
+++ src/test/java/org/example/BotTest.java
@@ -8,15 +8,15 @@ import org.mockito.Mock;
 import java.util.*;
 import static org.mockito.Mockito.*;
 
-public class BotTest implements BotTestInterface{
+public class BotTest implements BotTestInterface {
 
     private long ChatId;
 
-    private  MessageHandling bot ;
+    private MessageHandling bot;
 
     private PuzzleGame puzzleGame;
 
-    private Map<String, Puzzle>  puzzles;
+    private Map<String, Puzzle> puzzles;
 
     @Mock
     private Storage storage;
@@ -38,7 +38,7 @@ public class BotTest implements BotTestInterface{
      */
     @Test
     public void GenreCommandTest() {
-        String response = bot.parseMessage("/genre",ChatId);
+        String response = bot.parseMessage("/genre", ChatId);
         Assert.assertEquals("Здравствуйте, добро пожаловать в бот рекомендации книг! Нажмите /chat и выберите жанр", response);
 
         /*
@@ -151,7 +151,7 @@ public class BotTest implements BotTestInterface{
     public void testGetReadBooksCommandWithEmptyList() {
         ArrayList<String> emptyList = new ArrayList<>();
         when(storage.getReadBooks(ChatId)).thenReturn(emptyList);
-        String response = messageHandling.parseMessage("/getread",ChatId);
+        String response = messageHandling.parseMessage("/getread", ChatId);
         verify(storage, times(1)).getReadBooks(ChatId);
         Assert.assertEquals("Список прочитанных книг пуст.", response);
     }
@@ -166,7 +166,7 @@ public class BotTest implements BotTestInterface{
         nonEmptyList.add("Book 1");
         nonEmptyList.add("Book 2");
         when(storage.getReadBooks(ChatId)).thenReturn(nonEmptyList);
-        String response = messageHandling.parseMessage("/getread",ChatId);
+        String response = messageHandling.parseMessage("/getread", ChatId);
         verify(storage, times(1)).getReadBooks(ChatId);
         Assert.assertEquals("Прочитанные книги:\n1. Book 1\n2. Book 2\n", response);
     }
@@ -182,8 +182,8 @@ public class BotTest implements BotTestInterface{
         books.add("Book 1");
         books.add("Book 2");
         when(storage.getBooksByAuthor(author, ChatId)).thenReturn(books);
-        messageHandling.parseMessage("/getbyauthor",ChatId);
-        String response = messageHandling.parseMessage(author,ChatId);
+        messageHandling.parseMessage("/getbyauthor", ChatId);
+        String response = messageHandling.parseMessage(author, ChatId);
         verify(storage, times(1)).getBooksByAuthor(author, ChatId);
         Assert.assertEquals("Книги автора John Doe:\n" + "\"Book 1\";\n" + "\"Book 2\";\n", response);
     }
@@ -196,8 +196,8 @@ public class BotTest implements BotTestInterface{
     public void testGetBooksByAuthorCommandWithNoBooks() {
         String author = "Nonexistent Author";
         when(storage.getBooksByAuthor(author, ChatId)).thenReturn(new ArrayList<>());
-        messageHandling.parseMessage("/getbyauthor",ChatId);
-        String response = messageHandling.parseMessage(author,ChatId);
+        messageHandling.parseMessage("/getbyauthor", ChatId);
+        String response = messageHandling.parseMessage(author, ChatId);
         verify(storage, times(1)).getBooksByAuthor(author, ChatId);
         Assert.assertEquals("Нет прочитанных книг этого автора.", response);
     }
@@ -210,8 +210,8 @@ public class BotTest implements BotTestInterface{
     public void testGetBooksByYearCommandWithNoBooks() {
         int year = 1112;
         when(storage.getBooksByYear(year, ChatId)).thenReturn(new ArrayList<>());
-        messageHandling.parseMessage("/getbyyear",ChatId);
-        String response = messageHandling.parseMessage(String.valueOf(year),ChatId);
+        messageHandling.parseMessage("/getbyyear", ChatId);
+        String response = messageHandling.parseMessage(String.valueOf(year), ChatId);
         verify(storage, times(1)).getBooksByYear(year, ChatId);
         Assert.assertEquals("Нет прочитанных книг в этом году.", response);
     }
@@ -226,8 +226,8 @@ public class BotTest implements BotTestInterface{
         books.add("Book 1");
         books.add("Book 2");
         when(storage.getBooksByYear(year, ChatId)).thenReturn(books);
-        messageHandling.parseMessage("/getbyyear",ChatId);
-        String response = messageHandling.parseMessage(String.valueOf(year),ChatId);
+        messageHandling.parseMessage("/getbyyear", ChatId);
+        String response = messageHandling.parseMessage(String.valueOf(year), ChatId);
         verify(storage, times(1)).getBooksByYear(year, ChatId);
         Assert.assertEquals("Книги 2020 года:\n" + "\"Book 1\";\n" + "\"Book 2\";\n", response);
     }
@@ -243,8 +243,8 @@ public class BotTest implements BotTestInterface{
         readBooks.add("Book 1");
         readBooks.add("Book 2");
         when(storage.getReadBooks(ChatId)).thenReturn(readBooks);
-        messageHandling.parseMessage("/removebook",ChatId);
-        String response = messageHandling.parseMessage(message,ChatId);
+        messageHandling.parseMessage("/removebook", ChatId);
+        String response = messageHandling.parseMessage(message, ChatId);
         verify(storage, times(1)).updateReadBooks(eq(ChatId), any(ArrayList.class));
         Assert.assertEquals("Книга Book 1 успешно удалена из списка прочитанных!", response);
     }
@@ -260,8 +260,8 @@ public class BotTest implements BotTestInterface{
         readBooks.add("Book 1");
         readBooks.add("Book 2");
         when(storage.getReadBooks(ChatId)).thenReturn(readBooks);
-        messageHandling.parseMessage("/removebook",ChatId);
-        String response = messageHandling.parseMessage(message,ChatId);
+        messageHandling.parseMessage("/removebook", ChatId);
+        String response = messageHandling.parseMessage(message, ChatId);
         verify(storage, never()).updateReadBooks(eq(ChatId), any(ArrayList.class));
         Assert.assertEquals("Указанный уникальный номер книги не существует в списке прочитанных книг.", response);
     }
@@ -273,8 +273,8 @@ public class BotTest implements BotTestInterface{
     @Test
     public void testRemoveBookCommandWithInvalidFormat() {
         String message = "InvalidNumber";
-        messageHandling.parseMessage("/removebook",ChatId);
-        String response = messageHandling.parseMessage(message,ChatId);
+        messageHandling.parseMessage("/removebook", ChatId);
+        String response = messageHandling.parseMessage(message, ChatId);
         verify(storage, never()).updateReadBooks(eq(ChatId), any(ArrayList.class));
         Assert.assertEquals("Некорректный формат номера книги.", response);
     }
@@ -323,7 +323,7 @@ public class BotTest implements BotTestInterface{
     public void testEditBookCommandWithInvalidDataFormat() {
         String message = "InvalidData";
         messageHandling.parseMessage("/editbook", ChatId);
-        String response = messageHandling.parseMessage( message, ChatId);
+        String response = messageHandling.parseMessage(message, ChatId);
         verify(storage, never()).editReadBook(anyString(), anyString(), anyInt(), anyString(), anyString(), anyInt(), eq(ChatId));
         Assert.assertEquals("Некорректный формат номера книги.", response);
     }
@@ -354,12 +354,12 @@ public class BotTest implements BotTestInterface{
      * содержится в списке всех загадок
      */
     @Test
-        public void testPlayPuzzleContainsRandomPuzzle() {
-            puzzles = puzzleGame.getPuzzles();
-            String response = bot.parseMessage("/playpuzzle",ChatId);
-            String puzzleQuestion = response.replace("Добро пожаловать в игру в загадки! Начнем.\nЗагадка: ", "");
-            Assert.assertTrue(puzzles.containsKey(puzzleQuestion));
-        }
+    public void testPlayPuzzleContainsRandomPuzzle() {
+        puzzles = puzzleGame.getPuzzles();
+        String response = bot.parseMessage("/playpuzzle", ChatId);
+        String puzzleQuestion = response.replace("Добро пожаловать в игру в загадки! Начнем.\nЗагадка: ", "");
+        Assert.assertTrue(puzzles.containsKey(puzzleQuestion));
+    }
 
     /**
      * Проверяет команду /gethint - получения подсказки в игре в загадки.
@@ -390,7 +390,7 @@ public class BotTest implements BotTestInterface{
     public void testAnotheRiddleContainsRandomPuzzle() {
         puzzles = puzzleGame.getPuzzles();
         bot.parseMessage("/playpuzzle", ChatId);
-        String response = bot.parseMessage("/anotheriddle",ChatId);
+        String response = bot.parseMessage("/anotheriddle", ChatId);
         String puzzleQuestion = response.replace("Следующая загадка: ", "");
         Assert.assertTrue(puzzles.containsKey(puzzleQuestion));
     }
@@ -415,7 +415,7 @@ public class BotTest implements BotTestInterface{
         puzzles = puzzleGame.getPuzzles();
         bot.parseMessage("/playpuzzle", ChatId);
         String response = bot.parseMessage("/restart", ChatId);
-        String puzzleQuestion = response.replace("Игра в загадки начата заново.\n"+"Добро пожаловать в игру в загадки! Начнем.\nЗагадка: ", "");
+        String puzzleQuestion = response.replace("Игра в загадки начата заново.\n" + "Добро пожаловать в игру в загадки! Начнем.\nЗагадка: ", "");
         Assert.assertTrue(puzzles.containsKey(puzzleQuestion));
     }
 
@@ -437,5 +437,203 @@ public class BotTest implements BotTestInterface{
     public void stopPuzzleCommandTest() {
         bot.parseMessage("/playpuzzle", ChatId);
         String response = bot.parseMessage("/stoppuzzle", ChatId);
-        Assert.assertEquals("Режим головоломки завершен.\nПравильных ответов: 0\n" + "Неправильных ответов: 20\n" + "Процент правильных ответов: 0.0%", response);}
+        Assert.assertEquals("Режим головоломки завершен.\nПравильных ответов: 0\n" + "Неправильных ответов: 20\n" + "Процент правильных ответов: 0.0%", response);
+    }
+
+    /**
+     * Проверка корректного ответа от бота при использовании команды /vote.
+     */
+    @Test
+    public void voteCommandTest() {
+        String response = bot.parseMessage("/vote", ChatId);
+        Assert.assertEquals("Выберите 3 понравившиеся книги, отправив их номера последовательно:\n" +
+                "1. KGBT+ \n" +
+                "Александр Пелевин\n" +
+                "2. TRANSHUMANISM INC. \n" +
+                "Виктор Пелевин\n" +
+                "3. Двадцать тысяч лье под водой \n" +
+                "Жюль Верн\n" +
+                "4. Вино из одуванчиков \n" +
+                "Рэй Брэдбери\n" +
+                "5. Мастер и Маргарита \n" +
+                "Михаил Булгаков\n" +
+                "6. The One. Единственный \n" +
+                "Джон Маррс\n" +
+                "7. Атлант расправил плечи: Часть 3 \n" +
+                "Айн Рэнд\n" +
+                "8. Невидимый гость \n" +
+                "Эльдар Сафин\n" +
+                "9. Тайная жизнь домашних животных 2 \n" +
+                "Стив С. Миллер\n" +
+                "10. Клуб убийств по четвергам \n" +
+                "Ричард Осман\n", response);
+    }
+
+    /**
+     * Проверка последовательного голосования пользователя и корректных ответов бота.
+     */
+    @Test
+    public void voteAfterCommandTest() {
+        bot.parseMessage("/vote", ChatId);
+        String response = bot.parseMessage("3", ChatId);
+        Assert.assertEquals("Вы выбрали книгу номер 3. Выберите еще 2 книг(и).", response);
+        response = bot.parseMessage("2", ChatId);
+        Assert.assertEquals("Вы выбрали книгу номер 2. Выберите еще 1 книг(и).", response);
+        response = bot.parseMessage("1", ChatId);
+        Assert.assertEquals("Спасибо за ваш голос!", response);
+    }
+
+    /**
+     * Проверка обработки некорректных вводов пользователя в процессе голосования.
+     */
+    @Test
+    public void voteAfterWrongEnterCommandTest() {
+        bot.parseMessage("/vote", ChatId);
+        String response = bot.parseMessage("3", ChatId);
+        Assert.assertEquals("Вы выбрали книгу номер 3. Выберите еще 2 книг(и).", response);
+        response = bot.parseMessage("3", ChatId);
+        Assert.assertEquals("Вы уже выбрали эту книгу. Выберите другую.", response);
+        response = bot.parseMessage("re", ChatId);
+        Assert.assertEquals("Введите число от 1 до 10.", response);
+        response = bot.parseMessage("19", ChatId);
+        Assert.assertEquals("Неверный номер книги. Выберите номер от 1 до 10.", response);
+        response = bot.parseMessage("2", ChatId);
+        Assert.assertEquals("Вы выбрали книгу номер 2. Выберите еще 1 книг(и).", response);
+        response = bot.parseMessage("1", ChatId);
+        Assert.assertEquals("Спасибо за ваш голос!", response);
+        response = bot.parseMessage("/vote", ChatId);
+        Assert.assertEquals("Если вы пытаетесь проголосовать повторно, то этого сделать нельзя. Если вы хотите переголосовать, нажмите /revote", response);
+    }
+
+    /**
+     * Проверка корректного отображения результатов голосования командой /voteresults.
+     */
+    @Test
+    public void voteResultsCommandTest() {
+        String response = bot.parseMessage("/voteresults", ChatId);
+        Assert.assertTrue(response.startsWith("1 KGBT+ \n" + "Александр Пелевин - "));
+    }
+
+    /**
+     * Проверка невозможности использования команды /revote до команды /vote.
+     */
+    @Test
+    public void revoteWrongCommandTest() {
+        String response = bot.parseMessage("/revote", ChatId);
+        Assert.assertEquals("Вы не можете использовать эту команду до использования /vote", response);
+    }
+
+    /**
+     * Проверка корректной работы команды /revote после завершения голосования.
+     */
+    @Test
+    public void revoteCompleteCommandTest() {
+        bot.parseMessage("/vote", ChatId);
+        bot.parseMessage("3", ChatId);
+        bot.parseMessage("2", ChatId);
+        bot.parseMessage("1", ChatId);
+        String response = bot.parseMessage("/voteresults", ChatId);
+        Assert.assertEquals("1 KGBT+ \n" +
+                "Александр Пелевин - 1 голосов\n" +
+                "2 TRANSHUMANISM INC. \n" +
+                "Виктор Пелевин - 1 голосов\n" +
+                "3 Двадцать тысяч лье под водой \n" +
+                "Жюль Верн - 1 голосов\n" +
+                "4 Вино из одуванчиков \n" +
+                "Рэй Брэдбери - 0 голосов\n" +
+                "5 Мастер и Маргарита \n" +
+                "Михаил Булгаков - 0 голосов\n" +
+                "6 The One. Единственный \n" +
+                "Джон Маррс - 0 голосов\n" +
+                "7 Атлант расправил плечи: Часть 3 \n" +
+                "Айн Рэнд - 0 голосов\n" +
+                "8 Невидимый гость \n" +
+                "Эльдар Сафин - 0 голосов\n" +
+                "9 Тайная жизнь домашних животных 2 \n" +
+                "Стив С. Миллер - 0 голосов\n" +
+                "10 Клуб убийств по четвергам \n" +
+                "Ричард Осман - 0 голосов\n", response);
+        response = bot.parseMessage("/revote", ChatId);
+        Assert.assertEquals("Выберите 3 понравившиеся книги, отправив их номера последовательно:\n" +
+                "1. KGBT+ \n" +
+                "Александр Пелевин\n" +
+                "2. TRANSHUMANISM INC. \n" +
+                "Виктор Пелевин\n" +
+                "3. Двадцать тысяч лье под водой \n" +
+                "Жюль Верн\n" +
+                "4. Вино из одуванчиков \n" +
+                "Рэй Брэдбери\n" +
+                "5. Мастер и Маргарита \n" +
+                "Михаил Булгаков\n" +
+                "6. The One. Единственный \n" +
+                "Джон Маррс\n" +
+                "7. Атлант расправил плечи: Часть 3 \n" +
+                "Айн Рэнд\n" +
+                "8. Невидимый гость \n" +
+                "Эльдар Сафин\n" +
+                "9. Тайная жизнь домашних животных 2 \n" +
+                "Стив С. Миллер\n" +
+                "10. Клуб убийств по четвергам \n" +
+                "Ричард Осман\n", response);
+        bot.parseMessage("6", ChatId);
+        bot.parseMessage("7", ChatId);
+        bot.parseMessage("8", ChatId);
+        response = bot.parseMessage("/voteresults", ChatId);
+        Assert.assertEquals("1 KGBT+ \n" +
+                "Александр Пелевин - 0 голосов\n" +
+                "2 TRANSHUMANISM INC. \n" +
+                "Виктор Пелевин - 0 голосов\n" +
+                "3 Двадцать тысяч лье под водой \n" +
+                "Жюль Верн - 0 голосов\n" +
+                "4 Вино из одуванчиков \n" +
+                "Рэй Брэдбери - 0 голосов\n" +
+                "5 Мастер и Маргарита \n" +
+                "Михаил Булгаков - 0 голосов\n" +
+                "6 The One. Единственный \n" +
+                "Джон Маррс - 1 голосов\n" +
+                "7 Атлант расправил плечи: Часть 3 \n" +
+                "Айн Рэнд - 1 голосов\n" +
+                "8 Невидимый гость \n" +
+                "Эльдар Сафин - 1 голосов\n" +
+                "9 Тайная жизнь домашних животных 2 \n" +
+                "Стив С. Миллер - 0 голосов\n" +
+                "10 Клуб убийств по четвергам \n" +
+                "Ричард Осман - 0 голосов\n", response);
+    }
+
+    /**
+     * Проверка сценария завершения голосования без явного лидера.
+     */
+    @Test
+    public void testEndOfVotingWithNoLeader() {
+        // Устанавливаем значение VOTING_END_DAY
+        int votingEndDay = 3;
+        bot.setVotingEndDay(votingEndDay);
+        // Вызываем ваш метод, который зависит от текущей даты
+        String response = bot.parseMessage("/vote", ChatId);
+        // Проверяем ожидаемый результат (ваша логика зависит от текущей даты)
+        Assert.assertEquals("Лидера голосования нет", response);
+    }
+
+    /**
+     * Проверка сценария завершения голосования с явным лидером.
+     */
+    @Test
+    public void testEndOfVoting() {
+        bot.parseMessage("/vote", ChatId);
+        bot.parseMessage("3", ChatId);
+        bot.parseMessage("2", ChatId);
+        bot.parseMessage("1", ChatId);
+        // Устанавливаем значение VOTING_END_DAY
+        int votingEndDay = 3;
+        bot.setVotingEndDay(votingEndDay);
+        // Вызываем ваш метод, который зависит от текущей даты
+        String response = bot.parseMessage("/vote", ChatId);
+        // Проверяем ожидаемый результат (ваша логика зависит от текущей даты)
+        Assert.assertEquals("Голосование окончено.Итоги голосования:\n" +
+                "Книга с наибольшим числом голосов:\n" +
+                "Двадцать тысяч лье под водой \n" +
+                "Жюль Верн\n" +
+                "Вы можете присоединиться к нам и начать читать вместе!\n", response);
+    }
 }
