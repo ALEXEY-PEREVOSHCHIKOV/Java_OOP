823352bd41a00a33e8e4e4a092b078104c3705f5
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-12-10 14:58:03 +0500

    Изменён интерфейс DateTimeProvider, добавлены javadoc

--- src/main/java/org/example/DateTimeProvider.java
+++ src/main/java/org/example/DateTimeProvider.java
@@ -2,6 +2,15 @@ package org.example;
 
 import java.time.LocalDateTime;
 
+/**
+ * Интерфейс, предоставляющий метод для получения текущего значения даты и времени.
+ */
 public interface DateTimeProvider {
+
+    /**
+     * Возвращает текущее системное значение даты и времени.
+     *
+     * @return Текущее системное значение даты и времени
+     */
     LocalDateTime getCurrentDateTime();
 }
