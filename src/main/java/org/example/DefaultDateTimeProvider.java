b3e38461c31676e55f6f4ab916305a2d09ac2dba
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-12-10 14:58:55 +0500

    Обновлён класс DefaultDateTimeProvider, добавлены javadoc

--- src/main/java/org/example/DefaultDateTimeProvider.java
+++ src/main/java/org/example/DefaultDateTimeProvider.java
@@ -2,8 +2,16 @@ package org.example;
 
 import java.time.LocalDateTime;
 
+/**
+ * Класс, предоставляющий текущее системное значение даты и времени в качестве реализации интерфейса DateTimeProvider.
+ */
 public class DefaultDateTimeProvider implements DateTimeProvider {
 
+    /**
+     * Возвращает текущее системное значение даты и времени.
+     *
+     * @return Текущее системное значение даты и времени
+     */
     @Override
     public LocalDateTime getCurrentDateTime() {
         return LocalDateTime.now();
